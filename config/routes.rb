0c494789cba834520ec717f7f962f36db9e683f9
ogawatti <ogawattim@gmail.com>
2025-06-15 11:45:12 +0900

    add notification for email

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,10 @@ Rails.application.routes.draw do
   resources :passwords, param: :token
   get "up" => "rails/health#show", as: :rails_health_check
 
-  resources :products
+  resources :products do
+    resources :subscribers, only: [ :create ]
+  end
+  resource :unsubscribe, only: [ :show ]
 
   root "products#index"
 end
